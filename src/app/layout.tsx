6af65d8f36d3a685720139d7ba584c9922903f6f
Master6776 <ko-belmega@t-online.de>
2026-08-01 08:50:53 +0200

    Name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "MyCipher Dashboard",
+  title: "My Master Dashboard",
   description: "Trading Dashboard",
 };
 
